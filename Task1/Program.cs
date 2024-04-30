10e4aa44a17dc7960977ac66ab63db94156e79ce
Trofimchik Ewgeni <6978451ewgen@mail.ru>
2024-04-30 18:47:27 +0300

    Добавил решение программы

---
+++ Task1/Program.cs
@@ -0,0 +1,69 @@
+﻿// Задача: Написать программу, которая из имеющегося массива 
+// строк формирует новый массив из строк, длина которых меньше, 
+// либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма. 
+// При решении не рекомендуется пользоваться коллекциями, 
+// лучше обойтись исключительно массивами.
+
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+void Main()
+{
+    Console.Write("Введите количество элементов массива: ");
+    int m = Convert.ToInt32(Console.ReadLine());
+    string[] Array1 = new string[m];
+    Array(Array1);
+    string[] Array2 = new string[IfStringSymbol3(Array1)];
+    IfStringArray3(Array1, Array2);
+    printArray(Array1);
+    System.Console.Write("  =>  ");
+    printArray(Array2);
+}
+
+void Array(string[] stringArray)
+{
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} элемент массива");
+        stringArray[i] = Console.ReadLine()!;
+    }
+}
+
+int IfStringSymbol3(string[] stringArray)
+{
+    int n = 0;
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        if (stringArray[i].Length <= 3)
+            n++;
+    }
+    return n;
+}
+
+void IfStringArray3(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+
+void printArray(string[] stringArray)
+{
+    Console.Write("[");
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        Console.Write($"{stringArray[i]}, ");
+    }
+    Console.Write("]");
+}
+
+Main();
